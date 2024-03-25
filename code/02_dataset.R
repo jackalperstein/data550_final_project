27132b2c29b1c09bdd40bc34deeaf4a18b6be0c3
jackalperstein <jack.alperstein@emory.edu>
2024-03-25 18:44:04 -0400

    updated codes

---
+++ code/02_dataset.R
@@ -0,0 +1,51 @@
+
+pacman::p_load(data.table, tidyverse, gtsummary)
+
+here::i_am("code/02_analysis.Rmd")
+
+# simple naming list of all 10 CSVs in data folder
+csv_files <- list.files(path = "../data/", pattern = "\\.csv$")
+
+# combine all identified files into a large list
+csv_list <- setNames(lapply(paste0("../data/", csv_files), fread), csv_files)
+
+
+# convert variables for each CSV (which does not retain factor information)
+convert_variables <- function(df) {
+  df %>%
+    mutate(PATCO = factor(case_when(
+      PATCO == "Los_Angeles" ~ "Los Angeles",
+      PATCO == "San_Bernardino" ~ "San Bernardino",
+      TRUE ~ PATCO
+    )),
+    SEX = factor(SEX),
+    RACE = factor(RACE),
+    ETH = factor(ETH),
+    PAYOR = factor(PAYOR),
+    PATZIP = factor(PATZIP),
+    LANG = factor(LANG),
+    HEAT = factor(HEAT),
+    FEI = factor(FEI),
+    CKD = factor(CKD),
+    AKI = factor(AKI),
+    RENAL = factor(RENAL),
+    HEAT1 = factor(HEAT1),
+    FEI1 = factor(FEI1),
+    CKD1 = factor(CKD1),
+    AKI1 = factor(AKI1),
+    RENAL1 = factor(RENAL1)
+    )
+}
+
+
+# apply function to each dataframe in the list
+csv_list_fac_lab <- lapply(csv_list, convert_variables)
+
+# create a single dataframe, combining each CSV in the list, add column to identify origin CSV and make it a factor
+dataset <- bind_rows(csv_list_fac_lab, .id = "CSV") %>% 
+  mutate(CSV = as.factor(CSV))
+
+# remove working variables, objects, functions
+rm(csv_files, csv_list, csv_list_fac_lab, convert_variables)
+
+saveRDS(dataset, file = here::here("data/dataset.Rds"))
---
+++ code/03_table_one.R
@@ -0,0 +1,39 @@
+pacman::p_load(data.table, tidyverse, gtsummary)
+
+here::i_am("code/03_table_one.R")
+
+dataset <- readRDS(here::here("data/dataset.rds")) %>%
+  mutate(ENG_SPA_OTH = as.factor(ifelse(LANG == "English", "English",
+                                        ifelse(LANG == "Spanish", "Spanish", "Other")))) %>% 
+  mutate(ENG_SPA_OTH = factor(ENG_SPA_OTH, levels = c("English", "Spanish", "Other")))
+
+table1 <- tbl_summary(
+  dataset,
+  by = ENG_SPA_OTH,
+  include = c(PATCO, SEX, AGE, RACE, ETH, PAYOR, HEAT, FEI, AKI, CKD, RENAL, HEAT1, FEI1, AKI1, CKD1, RENAL1),
+  label = list(
+    PATCO ~ "County",
+    SEX ~ "Sex",
+    AGE ~ "Age",
+    RACE ~ "Race",
+    ETH ~ "Ethnicity",
+    PAYOR ~ "Method of Payment",
+    HEAT ~ "All Heat Illnesses (HEAT)",
+    FEI ~ "Fluid Electrolyte Imbalance (FEI)",
+    AKI ~ "Acute Kidney Injury (AKI)",
+    CKD ~ "Chronic Kidney Disease (CKD)",
+    RENAL ~ "All Renal Diseases (RENAL)",
+    HEAT1 ~ "HEAT is Primary Reason for Visit",
+    FEI1 ~ "FEI is Primary Reason for Visit",
+    AKI1 ~ "AKI is Primary Reason for Visit",
+    CKD1 ~ "CKD is Primary Reason for Visit",
+    RENAL1 ~ "RENAL is Primary Reason for Visit"),
+  missing_text = "Missing") %>%  
+  add_overall(col_label = "**Total**, N = {n}") %>% 
+  bold_labels() %>% 
+  modify_footnote(update = everything() ~ NA) %>% 
+  modify_header(label = "**Variable**") %>% 
+  as_gt() %>% gt::tab_header(title = "Table 1. Descriptive Statistics",
+                             subtitle = "Baseline Demographic and Language Data, 2010 - 2019")
+
+saveRDS(table1, file = here::here("output/table1.Rds"))
